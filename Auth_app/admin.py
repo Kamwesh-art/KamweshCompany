4b06eaf006e7d82a948a2c8e728e5481f09185ba
Kamwesh-art <9274lucy@gmail.com>
2026-08-14 14:47:02 +0300

    django admin listing

--- Auth_app/admin.py
+++ Auth_app/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Users)
+@admin.register(Users)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display=('id','username',"department")
+    search_fields=('username','id')
+    # ordering=('id')
--- Staff_app/views.py
+++ Staff_app/views.py
@@ -4,13 +4,14 @@ from .serializers import *
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from Auth_app.models import Users
 
 
 # Create your views here.
 #possessions
 # adding possessions
 @api_view(['POST'])
-def addposessions(request,user_id):
+def addpossessions(request,user_id):
     data= request.data.copy()
     data["user"]= user_id
     serializer=PossessionSerializer(data=data)
@@ -31,19 +32,26 @@ def addposessions(request,user_id):
 #         return Response(serializer.data)
 #     return Response(serializer.errors)
 
-#getting possessions
-@api_view(['GET'])
-def getpossessions(request,user_id):
-    possessions=Possessions.objects.filter(user=request.user)
-    serializer=PossessionSerializer(possessions, many=True)
-    return Response(serializer.data)
-
+# #getting possessions
 # @api_view(['GET'])
-# @permission_classes([IsAuthenticated])
-# def getpossessions(request):
-#     possessions = Possessions.objects.filter(user=request.user)
-#     serializer = PossessionSerializer(possessions, many=True)
+# def getpossessions(request,user_id):
+#     possessions=Possessions.objects.filter(user=request.user)
+#     serializer=PossessionSerializer(possessions, many=True)
 #     return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getpossessions(request,user_id):
+    try:
+        user=Users.objects.get(id=user_id)
+        if not user:
+            return Response({'message':'User not found'})
+    except:
+        pass
+
+    possessions = Possessions.objects.filter(user=user)
+    serializer = PossessionSerializer(possessions, many=True)
+    return Response(serializer.data)
 
 @api_view(['PUT'])
 def updatepossessions(request, user_id):
